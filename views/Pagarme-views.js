87444580c2ac6d8c614df325391d413d1aa2b03e
matias96321 <matias_69914081@hotmail.com>
2020-11-30 17:00:30 -0300

    correção views

--- views/Pagarme-views.js
+++ views/Pagarme-views.js
@@ -2,19 +2,21 @@ module.exports = {
     TransacoesViews(dados = []){
         return dados.map(dados =>{
             return({
+
                 id:dados.id,
                 status:dados.status,
                 payment_method:dados.payment_method,
                 paid_amout:dados.paid_amout,
                 date_created:dados.date_created,
+
                 customer:{
-                    id:dados.customer.id,
+                    id:dados.customer.external_id,
                     name:dados.customer.name,
                     email:dados.customer.email,
                     country:dados.customer.country,
                     phone_numbers:dados.customer.phone_numbers
-                    
                 },
+
                 shipping:{
                     id:dados.shipping.address.id,
                     zipcode:dados.shipping.address.zipcode,
@@ -24,6 +26,7 @@ module.exports = {
                     expedited:dados.shipping.expedited,                        
                     delivery_date:dados.shipping.delivery_date
                 },
+                
                 items:dados.items
             })
             
